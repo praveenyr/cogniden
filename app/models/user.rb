29649feb8ab4505281199fc372f783728d815022
Brittany Martin <brittany.jill.martin@gmail.com>
2017-04-27 14:40:14 -0400

    Update adding new Rolify role with newer documentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :wikis
 
-  rolify :before_add => :before_add_method
+  after_create :assign_default_role
 
-  def before_add_method(role)
-    self.add_role :member
+  def assign_default_role
+    self.add_role(:member)
   end
-  
+
 end
