f1fb8f263b02c0e43551a7563f8db58a334e0c89
Praveen <praveenyr@gmail.com>
2017-04-27 15:03:34 +0000

    Problem with Rolify

--- Gemfile
+++ Gemfile
@@ -72,3 +72,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bootstrap-sass'
 gem 'figaro', '1.0'
+gem 'rolify'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :wikis
+
+  rolify :before_add => :before_add_method
+
+  def before_add_method(role)
+    self.add_role :member
+  end
+  
 end
