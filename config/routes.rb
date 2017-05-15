7fbfda3c0c9f04e71aaacfd01b42a1c5973cdfac
Brittany Martin <brittany.jill.martin@gmail.com>
2017-05-14 23:03:54 -0400

    Remove app/views/charges/downgrade.html.erb & POST directly from application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-  
+
   resources :wikis
 
   devise_for :users
-  
+
   resources :charges, only: [:new, :create]
-  
+
   get 'about' => 'welcome#about'
-  
+
   root 'welcome#index'
-  
+
   post 'charges/downgrade' => 'charges#downgrade', :as => 'downgrade'
-  get 'charges/downgrade' => 'charges#downgrade', :as => 'downgrade_charge'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
