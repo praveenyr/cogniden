ca8e9e0e3a511d8fa06818813463a4dba77ef7ac
Praveen Yallakara <praveenyr@gmail.com>
2017-07-14 18:21:06 -0700

    Added clientside JS to test CORS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,18 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+var crushmet = {};
+
+crushmet.report = function(eventName) {
+   var event = { event: { name: eventName }};
+
+   var request = new XMLHttpRequest();
+
+   request.open("POST", "http://localhost:3000/api/events", true);
+   request.setRequestHeader('Content-Type', 'application/json');
+   request.send(JSON.stringify(event));
+
+  };
+
+crushmet.report("sale");
